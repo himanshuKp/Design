6d67dbc0a726adb6789dad7f59e5e5259cc7ff17
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-02-09 12:20:23 +0530

    object methods

--- Js/fundamentals 2/script.js
+++ Js/fundamentals 2/script.js
@@ -63,42 +63,103 @@ console.log(friends.indexOf('Himanshu'));   // use strict comparison, return -1 
 
 // tip calculator challenge
 
-const bills = [125, 555, 44];
-const tips = [];
-const total = []
+// const bills = [125, 555, 44];
+// const tips = [];
+// const total = []
 
-calculateTips(bills);
+// calculateTips(bills);
 
-calculateTotal(bills, tips);
+// calculateTotal(bills, tips);
 
-console.log("Bills: "+bills);
-console.log("Tips: "+tips);
-console.log("Total bill: "+total);
+// console.log("Bills: "+bills);
+// console.log("Tips: "+tips);
+// console.log("Total bill: "+total);
 
-function calcTip(billTotal) {
-    var billWithTip =  0;
-    if(billTotal >= 50 && billTotal <=300){
-        billWithTip = billTotal*0.15;
-    } else {
-        billWithTip = billTotal*0.20;
-    }
-    return billWithTip;
-}
+// function calcTip(billTotal) {
+//     var billWithTip =  0;
+//     if(billTotal >= 50 && billTotal <=300){
+//         billWithTip = billTotal*0.15;
+//     } else {
+//         billWithTip = billTotal*0.20;
+//     }
+//     return billWithTip;
+// }
 
-function calculateTips(bills){
-    for(var i=0;i<bills.length;i++){
-        var tipValue = 0;
-        tipValue = calcTip(bills[i]);
-        tips.push(tipValue);
-    }
-}
+// function calculateTips(bills){
+//     for(var i=0;i<bills.length;i++){
+//         var tipValue = 0;
+//         tipValue = calcTip(bills[i]);
+//         tips.push(tipValue);
+//     }
+// }
 
-function calculateTotal(bills, tips){
-    for(var i=0;i<bills.length;i++){
-        for(var j=0;j<tips.length;j++){
-            if(i === j){
-                total.push(Number(bills[i])+Number(tips[j]));
-            }            
-        }
-    }
-}
+// function calculateTotal(bills, tips){
+//     for(var i=0;i<bills.length;i++){
+//         for(var j=0;j<tips.length;j++){
+//             if(i === j){
+//                 total.push(Number(bills[i])+Number(tips[j]));
+//             }            
+//         }
+//     }
+// }
+
+//object 
+// const himanshu = {
+//     firstName: 'himanshu',
+//     lastName: 'kandpal',
+//     birthYear: 1993,
+//     age: 30,
+//     role: 'teacher',
+//     friends: ['abhishek', 'himanshu'],
+//     hasDriverLicence: true,
+
+//     calcAge: function (){
+//         this.age = 2021 - this.birthYear;
+//         // return 2021 - this.birthYear;
+//         // return this.age;
+//     }
+// }
+
+// himanshu['twitter'] = `@himanshukp`;
+// console.log(himanshu.calcAge());
+
+// console.log(`${himanshu.firstName} is a ${himanshu.age} year old, and he has ${himanshu.hasDriverLicence ? "a": "no"} driver's licence.`);
+
+// using dot notation
+// console.log(himanshu.calcAge(`${himanshu.birthYear}`));
+
+
+// using brackets
+// console.log(himanshu['calcAge'](`${himanshu.birthYear}`));
+// console.log(himanshu['calcAge']());
+
+// console.log(himanshu.age);
+
+// dot notation to get value from object
+// console.log(himanshu.lastName);
+
+// bracket notation
+// here we can use any expression we like
+// console.log(himanshu['lastName']);
+
+// const nameKey = 'Name';
+// console.log(himanshu[`first`+nameKey]);
+// console.log(himanshu[`last`+nameKey]);
+
+// const interestedIn = prompt(`What do you want to know about ${himanshu.firstName}. Choose firstName, lastName, age, role or friends`);
+// console.log(himanshu[interestedIn]);
+// if(himanshu[interestedIn]){
+//     console.log(himanshu[interestedIn]);
+// } else {
+//     console.log('No such property');
+// }
+// adds twitter as key and @himanshukp as value to object himanshu
+
+// console.log(himanshu);
+
+// find the first name in the friends in object himanshu
+// console.log(`${himanshu.firstName} has ${himanshu.friends.length} friends, and his best friend is called ${himanshu.friends[0]}.`);
+
+
+
+
