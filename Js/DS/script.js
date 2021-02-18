3f5275808a1befa319b1b000f737c2ab4bd96408
Himanshu Kandpal <cse10324.sbit@gmail.com>
2021-02-18 10:44:21 +0530

    destructure object and array

--- Js/DS/script.js
+++ Js/DS/script.js
@@ -1,23 +1,23 @@
 "use strict";
 
 /*----------------- Destructuring Array ----------------*/
-const restraunt = {
-  name: "Classico italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
-};
+// const restraunt = {
+//   name: "Classico italiano",
+//   location: "Via Angelo Tavanti 23, Firenze, Italy",
+//   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+//   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+//   mainMenu: ["Pizza", "Pasta", "Risotto"],
+// };
 
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-// this can also be save as below
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr); //not destroying the original array
+// // this can also be save as below
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr); //not destroying the original array
 
 // const [first, second] = restraunt.categories;
 
@@ -26,7 +26,7 @@ console.log(arr); //not destroying the original array
 
 // console.log(first, second);
 
-let [main, , secondary] = restraunt.categories;
+// let [main, , secondary] = restraunt.categories;
 
 // switch element
 // const temp = main;
@@ -36,5 +36,97 @@ let [main, , secondary] = restraunt.categories;
 // console.log(main, secondary);
 
 // reassign variable (alternate)
-[secondary, main] = [main, secondary];
-console.log(main, secondary);
+// [secondary, main] = [main, secondary];
+// console.log(main, secondary);
+
+// const nested = [2, 3, [4, 5]];
+// const [i, , j] = nested;
+// console.log(i, j);
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+/******************************Destructuring Objects******************************/
+// const restaurant = {
+//   name: "Classico Italiano",
+//   location: "Via Angelo Tavanti 23, Firenze, Italy",
+//   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+//   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+//   mainMenu: ["Pizza", "Pasta", "Risotto"],
+//   openingHours: {
+//     thur: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0,
+//       close: 24,
+//     },
+//   },
+
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
+
+//   orderDelivery: function ({
+//     starterIndex = 1,
+//     mainIndex = 0,
+//     time = "22:00",
+//     address,
+//   }) {
+//     console.log(
+//       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+//     );
+//   },
+// };
+
+// restaurant.orderDelivery({
+//   time: "22:30",
+//   address: "Gali no 13, Paratha wali gali",
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
+
+// restaurant.orderDelivery({
+//   address: "sector -4, vasundhara",
+// });
+
+// to destructure object we use curly braces
+const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// rename variables
+// const {
+//   name: restaurantName,
+//   openingHours: hourse,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hourse, tags);
+
+// setting a default value if property not found
+// default values
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+// if we dont use empty array as menu = [], it will show undefined
+// const { menu, starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+// mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+// need to contain in curly braces
+// ({ a, b } = obj);
+// console.log(a, b);
+
+// nested objects
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
+
+// console.log(orderDelivery);
+
+/***************************The spread operator***************************/
